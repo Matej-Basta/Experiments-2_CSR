f29c0a052a8dc65385f73348b1155ecf5dc982c6
matej <matej.basta@email.cz>
2023-12-02 10:47:17 +0100

    initial structure

---
+++ src/Image.js
@@ -0,0 +1,5 @@
+const Image = ({url, alt}) => {
+    return <img onClick={() => console.log("user clicked on the element")} onMouseEnter={() => console.log("user hovered over the element")} src={url} alt={alt} />
+};
+
+export default Image;
